fc3eb5ba7c04855bc1220a02e6ca8fb935799213
Keegan Kelly <keegankelly1@gmail.com>
2016-09-05 22:55:38 -0500

    Set up initial html and css. Created api call to populate gifs

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	var topics = ["rick and morty", "aqua teen hunger force", "the boondocks", "metalocalypse", "venture bros", "robot chicken", "tim and eric", "moral orel"];
+	var $buttons = $("#buttons");
+	var $gifs = $("#gifs");
+
+
+	for (var i = 0; i < topics.length; i++){
+		var $button = $("<button class='btn btn-secondary'>");
+		$button.text(topics[i]);
+		$button.attr("value", topics[i]);
+		$($buttons).append($button);
+	};
+
+	$("button").click(function(){
+		console.log("click works");
+		var subject = $(this).text();
+		console.log(subject);
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + subject +"&api_key=dc6zaTOxFJmzC"; 
+		console.log(queryURL);
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		})
+		.done(function(response){
+			var results = response.data;
+			console.log(results);
+			for(var i = 0; i < results.length; i++){
+				$img = $("<img>");
+				$img.attr("src", results[i].images.fixed_height.url);
+				$gifs.prepend($img);
+
+
+
+			};
+		});
+	});
+});
